0f43109e165d76efaca178fad47e59ca786a46fd
DanArmor <39347109+DanArmor@users.noreply.github.com>
2023-02-05 14:48:21 +0300

    Init

---
+++ IPX.C
@@ -0,0 +1,69 @@
+#include "ipx.h"
+#include <dos.h>
+#include <stdio.h>
+
+
+void (far *IPXDrvInvoke) (void);
+
+int IPXInit(void){
+	int status = 0;
+
+	_AX = _IPX_CHECK_DRIVER;
+	geninterrupt(_IPX_MULTIPLEX_INT);
+	status = _AL;
+
+	IPXDrvInvoke = MK_FP(_ES, _DI);
+	return status;
+}
+
+int IPXOpenSocket(int socketType, int *socketNum){
+	int status = 0;
+	_DX = _REVERSE_BYTES(*socketNum);
+	_AL = socketType;
+	_BX = _IPXOpenSocket;
+	IPXDrvInvoke();
+	status = _AL;
+	*socketNum = _DX;
+	*socketNum = _REVERSE_BYTES(*socketNum);
+	return status;
+}
+
+void IPXCloseSocket(int socketNum){
+	_BX = _IPXCloseSocket;
+	_DX = _REVERSE_BYTES(socketNum);
+	IPXDrvInvoke();
+}
+
+void IPXGetLocalTarget(void far *buf){
+	_ES = FP_SEG(buf);
+	_SI = FP_OFF(buf);
+	_BX = _IPXGetLocalTarget;
+	IPXDrvInvoke();
+}
+
+void IPXListenForPacket(void far *ECB){
+	_BX = _IPXListenForPacket;
+	_ES = FP_SEG(ECB);
+	_SI = FP_OFF(ECB);
+	IPXDrvInvoke();
+}
+
+void IPXSendPacket(void far *ECB){
+	_BX = _IPXSendPacket;
+	_ES = FP_SEG(ECB);
+	_SI = FP_OFF(ECB);
+	IPXDrvInvoke();
+}
+
+void IPXRelinquishControl(void){
+	_BX = _IPXRelinquishControl;
+	IPXDrvInvoke();
+}
+
+int IPXCancelEvent(void far *ECB){
+	_BX = _IPXSendPacket;
+	_ES = FP_SEG(ECB);
+	_SI = FP_OFF(ECB);
+	IPXDrvInvoke();
+	return _AL;
+}
--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # dos-ipx
 
-IPX documentation: https://www.novell.com/documentation/nw6p/pdfdoc/ipx_enu/ipx_enu.pdf
+Simple implementation of basic functions of `IPX protocol` for `DOS`.
 
+You can compile it yourself with `TurboC` (tested for version `2.01`).
 
+Provided compile version in tags as `.lib`.
+
+If you want to read some good IPX code - [DOOM(1993)](https://github.com/id-Software/DOOM/tree/master/ipx) is using it for multiplayer game.
+
+Format of packet:
+![ipx_header](readme/ipx_header.jpg)
+
+Format of ECB - application use it for sending/receiving packet.
+![ipx_ecb](readme/ipx_ecb.jpg)
+
+## Resources
+
+* [IPX Documentation](https://www.novell.com/documentation/nw6p/pdfdoc/ipx_enu/ipx_enu.pdf)
+* [IPX (RU)](https://frolov-lib.ru/books/bsp.old/v08/ch2.htm)
+* [TurboC User Guid](http://bitsavers.informatik.uni-stuttgart.de/pdf/borland/turbo_c/Turbo_C_Users_Guide_1987.pdf)
